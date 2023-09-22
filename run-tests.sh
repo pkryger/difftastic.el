64a859671b306329da3e6419aa772ddc90e39cdb
Przemyslaw Kryger <pkryger@Przemyslaws-MacBook-Air.local>
2023-09-22 14:47:50 +0100

    Fix indent and comment order in run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -27,10 +27,10 @@ INIT_PACKAGE_EL="(progn \
       (package-install pkg))))"
 
 if [[ ${mode} =~ ^(all|package-init)$ ]]; then
-# Refresh package archives, because the test suite needs to see at least
-# ${NEEDED_PACKAGES}.
-"$EMACS" -Q --batch \
-         --eval "$INIT_PACKAGE_EL"
+    # Refresh package archives, because the test suite needs to see at least
+    # ${NEEDED_PACKAGES}.
+    "$EMACS" -Q --batch \
+             --eval "$INIT_PACKAGE_EL"
 fi
 
 # Byte compile, failing on byte compiler errors, or on warnings unless ignored
@@ -41,24 +41,24 @@ else
 fi
 
 if [[ ${mode} =~ ^(all|byte-compile)$ ]]; then
-"$EMACS" -Q --batch \
-         --eval "$INIT_PACKAGE_EL" \
-         --load difftastic.el \
-         --eval "(setq byte-compile-error-on-warn ${ERROR_ON_WARN})" \
-         --funcall batch-byte-compile \
-         difftastic.el
+    "$EMACS" -Q --batch \
+             --eval "$INIT_PACKAGE_EL" \
+             --load difftastic.el \
+             --eval "(setq byte-compile-error-on-warn ${ERROR_ON_WARN})" \
+             --funcall batch-byte-compile \
+             difftastic.el
 fi
 
-if [[ ${mode} =~ ^(all|package-lint)$ ]]; then
 # Lint ourselves
 # Lint failures are ignored if EMACS_LINT_IGNORE is defined, so that lint
 # failures on Emacs 24.2 and below don't cause the tests to fail, as these
 # versions have buggy imenu that reports (defvar foo) as a definition of foo.
-"$EMACS" -Q --batch \
-         --eval "$INIT_PACKAGE_EL" \
-         -L . \
-         --eval "(require 'package-lint)" \
-         --eval '(setq package-lint-batch-fail-on-warnings nil)' \
-         --funcall package-lint-batch-and-exit \
-         difftastic.el || [ -n "${EMACS_LINT_IGNORE+x}" ]
+if [[ ${mode} =~ ^(all|package-lint)$ ]]; then
+    "$EMACS" -Q --batch \
+             --eval "$INIT_PACKAGE_EL" \
+             -L . \
+             --eval "(require 'package-lint)" \
+             --eval '(setq package-lint-batch-fail-on-warnings nil)' \
+             --funcall package-lint-batch-and-exit \
+             difftastic.el || [ -n "${EMACS_LINT_IGNORE+x}" ]
 fi

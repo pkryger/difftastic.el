87179bb64115dda17644a7ead2050cc49f56fe9f
Przemysław Kryger <pkryger@gmail.com>
2025-03-16 08:54:19 +0100

    build: Disable indent-tabs-mode only for emacs-lisp-mode
    Setting `indent-tabs-mode` globally interferes with `makefile-mode`, which
    requires tabs to declare recipes.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,9 @@
 ((nil . ((ispell-local-dictionary . "american")
-         (indent-tabs-mode . nil)
          (sentence-end-double-space . t)
          (fill-column . 79)
          (eval . (when (and (boundp 'projectile-project-types)
                             (alist-get 'pk/emacs-package projectile-project-types))
                    (setq projectile-project-type 'pk/emacs-package)))))
- (emacs-lisp-mode . ((eval . (progn
+ (emacs-lisp-mode . ((indent-tabs-mode . nil)
+                     (eval . (progn
                                (setq flycheck-emacs-lisp-load-path 'inherit))))))

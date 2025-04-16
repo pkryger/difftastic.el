pkryger/difftastic.el

commit ffa3a0ac75ed2533f4814bf7bbd3ec022c6f1eaa

refactor: Allow to bind to symbols in difftastic--with-temp-files
This makes the macro closer to what reqular `let*` would do, that is to bind
the file-bufs when they are declared. Switch to `let*` has been dictated by a
need to delete temporary files that could have been created while binding and
one of the following valueforms signals an error.

difftastic.el
@@ -1322,15 +1322,43 @@ argument."
        (difftastic--delete-temp-file-buf file-buf-B)))))
 
 (defmacro difftastic--with-file-bufs (file-bufs &rest body)
-  "Exectue the forms in BODY with each symbol in FILE-BUFS list `let'-bound to nil.
-If any form in BODY signals an error each element in FILE-BUFS will be passed
-to `difftastic--delete-temp-file-buf'."
+  "Exectue the forms in BODY with each spec in FILE-BUFS list `let'-bound.
+Each element in FILE-BUFS is in a form of SYMBOL (which is a
+symbol bound to nil) or a list (SYMBOL VALUEFORM) (which binds
+SYMBOL to the value of VALUEFORM).  Each VALUEFORM value is bound
+sequentially and can reffer to symbols already bound by this
+FILE-BUFS.  If any of VALUEFORM or any form in BODY signals an
+error each symbol in FILE-BUFS will be passed to
+`difftastic--delete-temp-file-buf'."
   (declare (indent 1) (debug ((&rest symbolp) body)))
-  `(let ,file-bufs
+  `(let ,(mapcar (lambda (file-buf)
+                   (cond
+                    ((symbolp file-buf) file-buf)
+                    ((and (listp file-buf)
+                          (equal (length file-buf) 2)
+                          (symbolp (car file-buf)))
+                     (car file-buf))
+                    (t (error (concat "Wrong type argument: symbolp or "
+                                      "(SYMBOL VALUEFORM), %S")
+                              file-buf))))
+                 file-bufs)
      (condition-case err
-         (progn ,@body)
+         (progn
+           ,(apply #'append
+                   '(setq) ; empty `setq' should be fine
+                   (mapcar (lambda (file-buf)
+                             (when (listp file-buf)
+                               file-buf))
+                           file-bufs))
+           ,@body)
        (error
-        (dolist (file-buf (list ,@file-bufs))
+        (dolist (file-buf
+                 (list ,@(mapcar
+                          (lambda (file-buf)
+                            (cond
+                             ((symbolp file-buf)  file-buf)
+                             ((listp file-buf) (car file-buf))))
+                          file-bufs)))
           (difftastic--delete-temp-file-buf file-buf))
         (signal (car err) (cdr err))))))
 
@@ -1371,9 +1399,10 @@ BUFFER-B is a file buffer,
 then ask for language before running difftastic."
   (interactive (difftastic--buffers-args))
 
-  (difftastic--with-file-bufs (file-buf-A file-buf-B)
-    (setq file-buf-A (difftastic--get-file-buf "A" (get-buffer buffer-A))
-          file-buf-B (difftastic--get-file-buf "B" (get-buffer buffer-B)))
+  (difftastic--with-file-bufs ((file-buf-A (difftastic--get-file-buf
+                                            "A" (get-buffer buffer-A)))
+                               (file-buf-B (difftastic--get-file-buf
+                                             "B" (get-buffer buffer-B))))
     (difftastic--files-internal
      (get-buffer-create
       (concat "*difftastic " buffer-A " " buffer-B "*"))
@@ -1475,12 +1504,11 @@ temporary file or nil otherwise."
   "Implementation for `difftastic-rerun', which see."
   (if-let* (((eq major-mode 'difftastic-mode))
             (rerun-alist (copy-tree difftastic--rerun-alist)))
-      (difftastic--with-file-bufs (file-buf-A file-buf-B)
-        (let-alist rerun-alist
-          (setq file-buf-A
-                (difftastic--rerun-file-buf "A" .file-buf-A rerun-alist)
-                file-buf-B
-                (difftastic--rerun-file-buf "B" .file-buf-B rerun-alist))
+      (let-alist rerun-alist
+        (difftastic--with-file-bufs ((file-buf-A (difftastic--rerun-file-buf
+                                                  "A" .file-buf-A rerun-alist))
+                                     (file-buf-B (difftastic--rerun-file-buf
+                                                  "B" .file-buf-B rerun-alist)))
           (let* ((default-directory .default-directory)
                  (lang-override (or lang-override
                                     (alist-get 'lang-override rerun-alist)))

test/difftastic.t.el
@@ -127,7 +127,7 @@
       (when (and (cdr file-buf) (file-exists-p (car file-buf)))
         (delete-file (car file-buf))))))
 
-(ert-deftest difftastic--with-file-bufs:basic-case-temporary-file-bufs-are-preserved ()
+(ert-deftest difftastic--with-file-bufs:basic-temporary-file-bufs-are-preserved ()
   (let (temp-file-A temp-file-B temp-file-C)
     (unwind-protect
         (progn
@@ -153,7 +153,7 @@
         (when (file-exists-p temp-file)
           (delete-file temp-file))))))
 
-(ert-deftest difftastic--with-file-bufs:error-mode-temporary-file-bufs-are-deleted ()
+(ert-deftest difftastic--with-file-bufs:basic-body-error-temporary-file-bufs-are-deleted ()
   (let (temp-file-A temp-file-B temp-file-C)
     (unwind-protect
         (progn
@@ -175,6 +175,88 @@
         (when (file-exists-p temp-file)
           (delete-file temp-file))))))
 
+(ert-deftest difftastic--with-file-bufs:valueform-temporary-file-bufs-are-preserved ()
+  (let (temp-file-A temp-file-B temp-file-C)
+    (unwind-protect
+        (progn
+          (setq temp-file-A (make-temp-file "difftastic.t")
+                temp-file-B (make-temp-file "difftastic.t")
+                temp-file-C (make-temp-file "difftastic.t"))
+          (should
+           (equal 42
+                  (eval
+                   `(difftastic--with-file-bufs ((file-buf-A (cons ,temp-file-A t))
+                                                 (file-buf-B (cons ,temp-file-B nil))
+                                                 (file-buf-C (cons ,temp-file-C t)))
+                      (should (equal file-buf-A (cons ,temp-file-A t)))
+                      (should (equal file-buf-B (cons ,temp-file-B nil)))
+                      (should (equal file-buf-C (cons ,temp-file-C t)))
+                      42))))
+          (should (file-exists-p temp-file-A))
+          (should (file-exists-p temp-file-B))
+          (should (file-exists-p temp-file-C)))
+
+      (dolist (temp-file (list temp-file-A temp-file-B temp-file-C))
+        (when (file-exists-p temp-file)
+          (delete-file temp-file))))))
+
+(ert-deftest difftastic--with-file-bufs:valueform-body-error-temporary-file-bufs-are-deleted ()
+  (let (temp-file-A temp-file-B temp-file-C)
+    (unwind-protect
+        (progn
+          (setq temp-file-A (make-temp-file "difftastic.t")
+                temp-file-B (make-temp-file "difftastic.t")
+                temp-file-C (make-temp-file "difftastic.t"))
+          (should-error
+           (eval
+            `(difftastic--with-file-bufs ((file-buf-A (cons ,temp-file-A t))
+                                          (file-buf-B (cons ,temp-file-B nil))
+                                          (file-buf-C (cons ,temp-file-C t)))
+               (should (equal file-buf-A (cons ,temp-file-A t)))
+               (should (equal file-buf-B (cons ,temp-file-B nil)))
+               (should (equal file-buf-C (cons ,temp-file-C t)))
+               (signal 'error "test-error"))))
+          (should-not (file-exists-p temp-file-A))
+          (should (file-exists-p temp-file-B))
+          (should-not (file-exists-p temp-file-C)))
+
+      (dolist (temp-file (list temp-file-A temp-file-B temp-file-C))
+        (when (file-exists-p temp-file)
+          (delete-file temp-file))))))
+
+(ert-deftest difftastic--with-file-bufs:valueform-error-temporary-file-bufs-are-deleted ()
+  (let (temp-file-A temp-file-B temp-file-C)
+    (unwind-protect
+        (progn
+          (setq temp-file-A (make-temp-file "difftastic.t")
+                temp-file-B (make-temp-file "difftastic.t")
+                temp-file-C (make-temp-file "difftastic.t"))
+          (should-error
+           (eval
+            `(difftastic--with-file-bufs ((file-buf-A (cons ,temp-file-A t))
+                                          (file-buf-B (cons ,temp-file-B nil))
+                                          (file-buf-C (cons ,temp-file-C t))
+                                          (file-buf-D (signal 'error "test-error"))))))
+          (should-not (file-exists-p temp-file-A))
+          (should (file-exists-p temp-file-B))
+          (should-not (file-exists-p temp-file-C)))
+
+      (dolist (temp-file (list temp-file-A temp-file-B temp-file-C))
+        (when (file-exists-p temp-file)
+          (delete-file temp-file))))))
+
+(ert-deftest difftastic--with-file-bufs:valueform-error ()
+  (should-error (eval `(difftastic--with-file-bufs 42)))
+  (should-error (eval `(difftastic--with-file-bufs (nil))))
+  (should-error (eval `(difftastic--with-file-bufs (t))))
+  (should-error (eval `(difftastic--with-file-bufs (42))))
+  (should-error (eval `(difftastic--with-file-bufs ("foo"))))
+  (should-error (eval `(difftastic--with-file-bufs ((42 t)))))
+  (should-error (eval `(difftastic--with-file-bufs ((nil t)))))
+  (should-error (eval `(difftastic--with-file-bufs ((t t)))))
+  (should-error (eval `(difftastic--with-file-bufs (("foo" t)))))
+  (should-error (eval `(difftastic--with-file-bufs ((foo)))))
+  (should-error (eval `(difftastic--with-file-bufs ((foo bar baz))))))
 
 (ert-deftest difftastic--delete-temp-file-buf:nil-does-nothing ()
   (should-not (difftastic--delete-temp-file-buf nil)))

commit 50a77d4bda9e7ef3c9e13c8b4407e7ced32b5fd6

fix: Update debug declararation in difftastic--with-temp-files
Without this `unsercover` fails to properly parse the difftastic.el file
resulting in no coverage reports are being generated for the file and - in
consequence - the usage is incomplete in coveralls.io.

The bug has been introduced in ffa3a0ac75ed2533f4814bf7bbd3ec022c6f1eaa, when
`difftastic--with-temp-files` has been updated to support more flexible
argument list.

difftastic.el
@@ -1341,7 +1341,7 @@ sequentially and can reffer to symbols already bound by this
 FILE-BUFS.  If any of VALUEFORM or any form in BODY signals an
 error each symbol in FILE-BUFS will be passed to
 `difftastic--delete-temp-file-buf'."
-  (declare (indent 1) (debug ((&rest symbolp) body)))
+  (declare (indent 1) (debug ((&rest &or (sexp form) sexp) body)))
   `(let ,(mapcar (lambda (file-buf)
                    (cond
                     ((symbolp file-buf) file-buf)
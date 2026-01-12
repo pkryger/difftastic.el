b33554a22c637f147d07c15fa9539c72bcfcfca0
Przemysław Kryger <pkryger@gmail.com>
2026-01-12 14:01:54 +0000

    build: Release version 1.0.0

--- difftastic.el
+++ difftastic.el
@@ -10,7 +10,7 @@
 ;;     (compat "29.1.4.2")
 ;;     (magit "4.0.0")
 ;;     (transient "0.4.0"))
-;; Version: 0.0.0
+;; Version: 1.0.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
